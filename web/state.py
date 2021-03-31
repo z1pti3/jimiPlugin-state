35e4201801b78490c59a22fc8039f6658f5fce44
z1pti3 <z1pti3@outlook.com>
2021-03-31 19:32:26 +0100

    Updated web to support new prefix

--- web/state.py
+++ web/state.py
@@ -10,7 +10,7 @@ from plugins.state.models import state
 
 pluginPages = Blueprint('statePages', __name__, template_folder="templates")
 
-@pluginPages.route("/state/")
+@pluginPages.route("/")
 def mainPage():
     foundStates = state._state().query(sessionData=jimi.api.g.sessionData)["results"]
     return render_template("state.html", states=foundStates)
